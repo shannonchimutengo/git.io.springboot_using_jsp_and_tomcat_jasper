c405ac261599ebb1f34f0aef3453ab3680b11d55
shannon <shannonchimutengo@gmail.com>
2022-06-28 19:53:30 +0200

    updated result.jsp

--- src/main/java/com/example/java/and/jstl/boot/Controllers.java
+++ src/main/java/com/example/java/and/jstl/boot/Controllers.java
@@ -6,18 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
+import java.io.IOException;
 
 @Controller
 public class Controllers {
 
 @GetMapping("home")
 
-    public String show(HttpServletRequest request){
+    public String show(HttpServletRequest request, HttpServletResponse response) throws IOException {
 String  username = request.getParameter("name");
     HttpSession session = request.getSession();
     session.setAttribute("name",username);
+
+    response.sendRedirect("result.jsp");
+
+
         return"home";
     }
 }
