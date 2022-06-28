e208ecb0d4ae93b5d734227ec418985e56a1de33
shannon <shannonchimutengo@gmail.com>
2022-06-28 20:41:51 +0200

    updated main.css

--- src/main/java/com/example/java/and/jstl/boot/Controllers.java
+++ src/main/java/com/example/java/and/jstl/boot/Controllers.java
@@ -2,13 +2,10 @@ package com.example.java.and.jstl.boot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 import java.io.IOException;
 
 @Controller
@@ -17,13 +14,11 @@ public class Controllers {
 @GetMapping("home")
 
     public String show(HttpServletRequest request, HttpServletResponse response) throws IOException {
-String  username = request.getParameter("name");
+
+    String  username = request.getParameter("name");
     HttpSession session = request.getSession();
+
     session.setAttribute("name",username);
-
-    response.sendRedirect("result.jsp");
-
-
         return"home";
     }
 }
