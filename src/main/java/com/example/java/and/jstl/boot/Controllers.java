8b3b9052bdaab70e9e3edc8df7ff1003419538e4
shannonchimutengo <shannonchimutengo@gmail.com>
2022-06-29 00:43:32 +0000

    updated annotations

--- src/main/java/com/example/java/and/jstl/boot/Controllers.java
+++ src/main/java/com/example/java/and/jstl/boot/Controllers.java
@@ -2,6 +2,8 @@ package com.example.java.and.jstl.boot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controllers {
