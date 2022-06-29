3c6b606da1d3e67bc4247162125c335dd3e82f51
shannonchimutengo <shannonchimutengo@gmail.com>
2022-06-29 00:07:03 +0000

    updated pom.xml for gitpod to run

--- src/main/java/com/example/java/and/jstl/boot/Controllers.java
+++ src/main/java/com/example/java/and/jstl/boot/Controllers.java
@@ -2,8 +2,6 @@ package com.example.java.and.jstl.boot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Controllers {
