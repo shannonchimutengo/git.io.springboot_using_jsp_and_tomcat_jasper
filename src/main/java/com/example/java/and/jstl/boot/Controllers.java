0448250ecb9e12fea8b69f9d35dc5b32380bdbea
shannonchimutengo <shannonchimutengo@gmail.com>
2022-06-29 00:42:29 +0000

    commited

--- src/main/java/com/example/java/and/jstl/boot/Controllers.java
+++ src/main/java/com/example/java/and/jstl/boot/Controllers.java
@@ -2,16 +2,6 @@ package com.example.java.and.jstl.boot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-=======
->>>>>>> 3c6b606 (updated pom.xml for gitpod to run)
 
 @Controller
 public class Controllers {
